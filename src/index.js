8403df9949943ede36f17b69e7e4b8a8025522fc
foo <foo@bar.de>
2020-07-01 02:40:11 +0200

    feat: support babel

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import babelRegister from '@babel/register'
 import { filter, first, map } from '@dword-design/functions'
 import P from 'path'
 
 export default filename => {
   if (P.basename(filename) === 'nuxt.config.js') {
+    babelRegister()
     const config = require(filename)
     const modules = config.modules || []
     return (
